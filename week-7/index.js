a02959bbc7fd8921e3ae319d3d47bd60e98a36a7
ediiotero <e.otero81@gmail.com>
2025-03-20 19:35:22 -0400

    week 7 ready

---
+++ week-7/index.js
@@ -0,0 +1,35 @@
+// Write a function that asks the user for 3 numbers, adds them all up then multiply 
+// them by 3. You must check that the users input is a number if it isn't
+// prompt the user again
+
+
+
+
+// ============================================
+
+// Write a function called 'finishSentence' that takes two inputs,  an array and a function
+// use the list array below
+// your callback function called 'combine' should combine two strings together and return them
+// finishSentence should loop through the given array prompt the user with the questions in the array
+// and combine the elements in the array with the word given in the prompt and alert the answer
+//*** Bonus: accomplish the same result without the finishSentence function and use an array method
+
+const list = ["My name is ", "I live in ", "My favorite food is "];
+
+
+
+
+// ============================================
+
+// Write a Promise
+// The promise should prompt the user to guess a number between 1 and 20,
+// wait 3 seconds 
+// and check to see if the number is greater than or equal to 10
+// if its larger resolve the promise
+// if its lower reject the promise
+// Use the promise to alert "you win" if resolved
+// and "you lose" if rejected
+
+
+
+
---
+++ week-7/promise.js
@@ -0,0 +1,15 @@
+// Callbacks
+
+
+
+
+// Promise
+// more here: https://www.w3schools.com/js/js_promise.asp
+
+
+
+
+// ========================
+
+// Fetch Api 
+
